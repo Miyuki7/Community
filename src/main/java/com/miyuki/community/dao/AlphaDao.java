4444440e15c92272ce7d2030d0849de0ff1f401d
Miyuki <yuzh7799@gmail.com>
2022-11-16 19:12:14 +0800

    点赞关注实现

---
+++ src/main/java/com/miyuki/community/dao/AlphaDao.java
@@ -0,0 +1,7 @@
+package com.miyuki.community.dao;
+
+public interface AlphaDao {
+
+    String select();
+
+}
---
+++ src/main/java/com/miyuki/community/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,10 @@
+package com.miyuki.community.dao.elasticsearch;
+
+import com.miyuki.community.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+
+}
